defd6392760de543f46c428561bfc233e3e476c7
Mikko <mikko.j.valiaho@gmail.com>
2022-12-07 09:16:57 +0200

    Basic functionality.

---
+++ src/test/java/com/suber/suber/SuberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suber.suber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
